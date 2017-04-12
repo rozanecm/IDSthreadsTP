b18e5452f480b34cd2bd3ad399d4a2e3d4b7bb78
Matias Rozanec <rozanecm@gmail.com>
2017-04-12 19:05:02 -0300

    Agrego chequeo de errores en apertura de archivo

--- Sniffer.cpp
+++ Sniffer.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include "Sniffer.h"
 
 Sniffer::Sniffer(char *filePath){
     file.open(filePath);
+    if(!file.is_open()){
+        std::cout<<"Error opnening file";
+    }
+    
     file.close();
 }
