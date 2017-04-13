5baf1e05242fed3f27d72e3732e6982135e02f1a
Matias Rozanec <rozanecm@gmail.com>
2017-04-13 11:58:54 -0300

    se termino de implementar correctamente la lectura de un msj ip

--- Sniffer.cpp
+++ Sniffer.cpp
@@ -15,20 +15,24 @@ Sniffer::Sniffer(char *filePath){
 void Sniffer::parseFile(){
     const int maxMsgLength = 65535;
     const short headerLength = 20;
+    const int maxReadDataLength = maxMsgLength - headerLength;
     short totalMsgLength = 0;   //longitud total del msj que se esta leyendo
     short identificador = 0;
     short flags = 0;
+    short offset = 0;
+    int direccionOrigen = 0;
+    int direccionDestino = 0;
+    std::string readMsg;
 
-    /* buffer donde se guarda lo leido */
-    char buffer[maxMsgLength];
+    /* buffer donde se guarda el header leido */
+    char buffer[headerLength];
 
-    /* leo primeros dos bytes; descarto */
-    file.read(buffer, 2);
+    /* leo primeros header */
+    file.read(buffer, headerLength);
     /* read other two bytes, which are total msg length */
-    file.read(buffer, 2);
-    totalMsgLength |= buffer[0];
-    totalMsgLength = totalMsgLength << 1;
-    totalMsgLength |= buffer[1];
+    totalMsgLength |= buffer[2];
+    totalMsgLength = totalMsgLength << 8;
+    totalMsgLength |= buffer[3];
 
     short msgLength = totalMsgLength - headerLength;
 
@@ -37,19 +41,56 @@ void Sniffer::parseFile(){
     std::cout<<"msg length "<<msgLength<<std::endl;
 
     /* next two bytes are the identificador */
-    file.read(buffer, 2);
-    identificador |= buffer[0];
-    identificador = identificador << 1;
-    identificador |= buffer[1];
+    identificador |= buffer[4];
+    identificador = identificador << 8;
+    identificador |= buffer[5];
 
     std::cout<<"identificador: "<<identificador<<std::endl;
 
     /* next two bytes correspond to: flags(3 bits); offset(13 bits) */
-    file.read(buffer, 2);
     /* we're interested in the 3rd bit of the flags part */
-    flags |= buffer[3];
+    flags |= buffer[6];
+    flags = flags >> 1;
     std::cout<<"flags: "<<flags<<std::endl;
-    
+    //TODO quedara verificar si es par o impar (si es impar el ultimo bit es 1)
+//    std::cout<<"sizeof(short): "<<sizeof(short)<<std::endl;
+    offset |= buffer[6];
+    offset = offset << 8;
+    offset |= buffer[7];
+    offset = offset << (sizeof(short) * 8 - 5);
+    offset = offset >> (sizeof(short) * 8 - 5);
+    std::cout<<"offset: "<<offset<<std::endl;
+
+    /* ignore next four bytes */
+
+    /* next 4 bytes are direccoin origen*/
+    direccionOrigen |= buffer[12];
+    direccionOrigen = direccionOrigen << 8;
+    direccionOrigen |= buffer[13];
+    direccionOrigen = direccionOrigen << 8;
+    direccionOrigen |= buffer[14];
+    direccionOrigen = direccionOrigen << 8;
+    direccionOrigen |= buffer[15];
+    std::cout<<"dir origen: "<<direccionOrigen<<std::endl;
+
+    /* next 4 bytes are direccoin origen*/
+    direccionDestino |= buffer[16];
+    direccionDestino = direccionDestino << 8;
+    direccionDestino |= buffer[17];
+    direccionDestino = direccionDestino << 8;
+    direccionDestino |= buffer[18];
+    direccionDestino = direccionDestino << 8;
+    direccionDestino |= buffer[19];
+    std::cout<<"dir origen: "<<direccionDestino<<std::endl;
+
+    /* buffer donde se guarda el msj leido */
+    char msgBuffer[maxReadDataLength];
+    /* leo msj */
+    file.read(msgBuffer, msgLength);
+    msgBuffer[msgLength] = '\0';
+    readMsg = msgBuffer;
+
+    std::cout<<"Read msg: "<<readMsg<<std::endl;
 }
 
 Sniffer::~Sniffer(){
