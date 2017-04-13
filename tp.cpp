e2b632d93a053bbbe4506bc76151b78d097cd9eb
Matias Rozanec <rozanecm@gmail.com>
2017-04-13 10:30:28 -0300

    cout para verificar ruta de archvo leida; invocacion a lo desarrollado para poder ir verificando funcionamiento

--- tp.cpp
+++ tp.cpp
@@ -5,9 +5,11 @@ int main(int argc, char *argv[]) {
     /* chequeo cant. de parametros: debe haber al menos 3
      * (nombre del programa, reglas, y al menos una captura
      * */
-    if (argc < 3){
-        return 1;
-    }
+//    if (argc < 3){
+//        return 1;
+//    }
+//    std::cout <<argv[1]<< std::endl;
     Sniffer unSniffer(argv[1]);
+    unSniffer.parseFile();
     return 0;
 }
