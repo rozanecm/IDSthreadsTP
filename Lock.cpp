237d9ae406b8a149643ecdee8426e0735ff2004a
Matias Rozanec <rozanecm@gmail.com>
2017-04-20 11:19:10 -0300

    create Lock class

---
+++ Lock.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rozanecm on 4/20/17.
+//
+
+#include "Lock.h"
---
+++ Lock.h
@@ -0,0 +1,10 @@
+#ifndef PROYECTO_LOCK_H
+#define PROYECTO_LOCK_H
+
+
+class Lock {
+private:
+};
+
+
+#endif //PROYECTO_LOCK_H
