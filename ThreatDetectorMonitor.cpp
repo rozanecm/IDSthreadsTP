31a94e6b139018a366f5ece7809f6affbdad8385
Matias Rozanec <rozanecm@gmail.com>
2017-04-20 15:31:40 -0300

    completed ThreatDetectorMonitor class. Compile checked; runtime check missing

--- ThreatDetectorMonitor.cpp
+++ ThreatDetectorMonitor.cpp
@@ -1,1 +1,20 @@
 #include "ThreatDetectorMonitor.h"
+#include "Lock.h"
+
+ThreatDetectorMonitor::ThreatDetectorMonitor(ThreatDetector threatDetector1) :
+        threatDetector(threatDetector1){}
+
+void ThreatDetectorMonitor::addIPPacket(IPPacket packet) {
+    Lock l(m);
+    threatDetector.addIPPacket(packet);
+}
+
+void ThreatDetectorMonitor::checkForNewThreats() {
+    Lock l(m);
+    threatDetector.checkForNewThreats();
+}
+
+void ThreatDetectorMonitor::removeIPPacket(IPPacket packet) {
+    Lock l(m);
+    threatDetector.removeIPPacket(packet);
+}
--- ThreatDetectorMonitor.h
+++ ThreatDetectorMonitor.h
@@ -2,8 +2,25 @@
 #define PROYECTO_THREATDETECTORMONITOR_H
 
 
-class ThreatDetectorMonitor {
+#include <mutex>
+#include "IPPacket.h"
+#include "Rule.h"
+#include "ThreatDetector.h"
 
+class ThreatDetectorMonitor {
+private:
+    std::mutex m;
+
+    ThreatDetector &threatDetector;
+
+public:
+    ThreatDetectorMonitor(ThreatDetector threatDetector1);
+
+    void addIPPacket(IPPacket packet);
+
+    void checkForNewThreats();
+
+    void removeIPPacket(IPPacket packet);
 };
 
 
