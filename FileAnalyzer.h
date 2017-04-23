c7f05d21ebf044ae8a1c16b7b323a4da5d103662
Matias Rozanec <rozanecm@gmail.com>
2017-04-23 02:48:39 -0300

    add attributes

--- FileAnalyzer.h
+++ FileAnalyzer.h
@@ -2,12 +2,22 @@
 #define PROYECTO_FILEANALYZER_H
 
 
+#include "Thread.h"
+#include "Sniffer.h"
+#include "ThreatDetectorMonitor.h"
+#include "AssemblerMonitor.h"
+
 class FileAnalyzer : public Thread{
     Sniffer unSniffer;
 
     ThreatDetectorMonitor &threatDetectorMonitor;
 
     AssemblerMonitor &assemblerMonitor;
+
+public:
+    FileAnalyzer(char* filepath,
+                 ThreatDetectorMonitor &threatDetectorMonitor1,
+                 AssemblerMonitor assemblerMonitor1);
 };
 
 
