20c6aee615c150409ec89d0d8639865d8b4c9828
Matias Rozanec <rozanecm@gmail.com>
2017-04-23 00:12:14 -0300

    completed Assembler monitor class

--- AssemblerMonitor.cpp
+++ AssemblerMonitor.cpp
@@ -1,1 +1,20 @@
 #include "AssemblerMonitor.h"
+#include "Lock.h"
+
+AssemblerMonitor::AssemblerMonitor(Assembler assembler1) :
+        assembler(assembler1){}
+
+void AssemblerMonitor::addPacket(IPPacket packet) {
+    Lock l(m);
+    assembler.addPacket(packet);
+}
+
+IPPacket AssemblerMonitor::getWholePacketFor(IPPacket packet) {
+    Lock l(m);
+    return assembler.getWholePacketFor(packet);
+}
+
+bool AssemblerMonitor::fragmentsCompleteWholeMessage(IPPacket *packet) {
+    Lock l(m);
+    return assembler.fragmentsCompleteWholeMessage(packet);
+}
--- AssemblerMonitor.h
+++ AssemblerMonitor.h
@@ -2,8 +2,23 @@
 #define PROYECTO_ASSEMBLERMONITOR_H
 
 
+#include <mutex>
+#include "Assembler.h"
+
 class AssemblerMonitor {
-    
+private:
+    std::mutex m;
+
+    Assembler &assembler;
+
+public:
+    AssemblerMonitor (Assembler assembler1);
+
+    void addPacket(IPPacket packet);
+
+    IPPacket getWholePacketFor(IPPacket packet);
+
+    bool fragmentsCompleteWholeMessage(IPPacket *packet);
 };
 
 
