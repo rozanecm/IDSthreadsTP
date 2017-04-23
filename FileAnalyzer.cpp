4e332ff586a3ca5997a72c4e16d7c49612f183ce
Matias Rozanec <rozanecm@gmail.com>
2017-04-23 03:00:03 -0300

    add constructor

--- FileAnalyzer.cpp
+++ FileAnalyzer.cpp
@@ -1,1 +1,8 @@
 #include "FileAnalyzer.h"
+
+FileAnalyzer::FileAnalyzer(char *filepath,
+                           ThreatDetectorMonitor &threatDetectorMonitor1,
+                           AssemblerMonitor assemblerMonitor1)
+        : unSniffer(Sniffer(filepath)),
+          threatDetectorMonitor(threatDetectorMonitor1),
+          assemblerMonitor(assemblerMonitor1){}
